aa1a4d51cdc9bb6f558e1bffe01042520402596d
Charles Knipp <charles.a.knipp@frb.gov>
2026-06-08 14:22:21 -0400

    added static arrays to demo

--- layered_closures/demo.jl
+++ layered_closures/demo.jl
@@ -3,6 +3,7 @@ using SSMProblems
 using LinearAlgebra
 using Printf
 using Random
+using StaticArrays
 
 include("kalman_filter.jl")
 include("linear_gaussian.jl")
@@ -12,23 +13,25 @@ include("conditional.jl")
 
 # the volatility process is linear in the log space
 function random_walk(γ::T) where {T<:Real}
-    return LinearGaussianDynamics(I(2), zeros(T, 2), γ * I(2))
+    return LinearGaussianDynamics(
+        SMatrix{2,2,T}(I), zeros(SVector{2,T}), γ * SMatrix{2,2,T}(I)
+    )
 end
 
 # this is just for consistency among definitions
 function conditional_prior(::T) where {T<:Real}
     function inner_process(state; kwargs...)
-        return GaussianPrior(zeros(T, 1), T(100) * I(1))
+        return GaussianPrior(zeros(SVector{1,T}), 100 * SMatrix{1,1,T}(I))
     end
     return inner_process
 end
 
 # closes over A and b
 function conditional_dynamics(::T) where {T<:Real}
-    A = ones(T, 1, 1)
-    b = zeros(T, 1)
+    A = ones(SMatrix{1,1,T})
+    b = zeros(SVector{1,T})
     function inner_process(state; kwargs...)
-        Q = T[exp(state[1]);;]
+        Q = SMatrix{1,1,T}(exp(state[1]))
         return LinearGaussianDynamics(A, b, Q)
     end
     return inner_process
@@ -36,10 +39,10 @@ end
 
 # closes over H and c
 function conditional_observation(::T) where {T<:Real}
-    H = ones(T, 1, 1)
-    c = zeros(T, 1)
+    H = ones(SMatrix{1,1,T})
+    c = zeros(SVector{1,T})
     function inner_process(state; kwargs...)
-        R = T[exp(state[2]);;]
+        R = SMatrix{1,1,T}(exp(state[2]))
         return LinearGaussianObservation(H, c, R)
     end
     return inner_process
@@ -48,7 +51,9 @@ end
 # generate the whole model
 function stochastic_volatility_model(γ::T) where {T<:Real}
     return StateSpaceModel(
-        ConditionalPrior(GaussianPrior(zeros(T, 2), T(10) * I(2)), conditional_prior(γ)),
+        ConditionalPrior(
+            GaussianPrior(zeros(SVector{2,T}), 10 * SMatrix{2,2,T}(I)), conditional_prior(γ)
+        ),
         ConditionalDynamics(random_walk(γ), conditional_dynamics(γ)),
         ConditionalObservation(conditional_observation(γ)),
     )
